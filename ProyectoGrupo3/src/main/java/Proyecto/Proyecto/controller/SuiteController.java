7f0ec26ffb504bb7b1b4d5fc2b76258f0a240d82
AndyHR14 <andypipoh@gmail.com>
2025-03-10 01:00:51 -0600

    Suite terminado
    Realizados los cambios en Suite

--- ProyectoGrupo3/src/main/java/Proyecto/Proyecto/controller/SuiteController.java
+++ ProyectoGrupo3/src/main/java/Proyecto/Proyecto/controller/SuiteController.java
@@ -2,13 +2,57 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 package Proyecto.Proyecto.controller;
 
-/**
- *
- * @author Hp
- */
-public class SuiteController {
+import Proyecto.Proyecto.domain.Suite;
+import Proyecto.Proyecto.service.SuiteService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+@Slf4j
+@RequestMapping("/suite")
+public class SuiteController {
+    
+    @Autowired
+    private SuiteService suiteService;
+
+    @GetMapping("/listado")
+    public String inicio(Model model) {
+        var suite = suiteService.getSuites(false);
+        model.addAttribute("suite", suite);
+        model.addAttribute("totalSuite", suite.size());
+        return "/suite/listado";
+    }
+  
+     @GetMapping("/nuevo")
+    public String suiteNuevo(Suite suite) {
+        return "/suite/modifica";
+    }
+
+    @Autowired
+    private FirebaseStorageServiceImpl firebaseStorageService;
+    
+    @PostMapping("/guardar")
+    public String suiteGuardar(Suite suite){
+        suiteService.save(suite);
+        return "redirect:/suite/listado";
+    }
+
+    @GetMapping("/eliminar/{id}")
+    public String suiteEliminar(Suite suite) {
+        suiteService.delete(suite);
+        return "redirect:/suite/listado";
+    }
+
+    @GetMapping("/modificar/{id}")
+    public String suiteModificar(Suite suite, Model model) {
+        suite = suiteService.getSuite(suite);
+        model.addAttribute("suite", suite);
+        return "/suite/modifica";
+    }   
 }
