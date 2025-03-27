29bf01773e018b7d499613bd22a25e66de8b566a
Sebastian Vasquez <svasvil0211@gmail.com>
2025-03-27 16:27:43 -0600

    DropDown y tablas
    Modifiqué las tablas , esta el dropdonw y cambio en la base de datos

--- ProyectoGrupo3/src/main/resources/CrearTablas.sql
+++ ProyectoGrupo3/src/main/resources/CrearTablas.sql
@@ -11,45 +11,22 @@ FLUSH PRIVILEGES;
 USE hotel;
 
 -- Creación de las Tablas del Hotel con el campo 'activo'
-CREATE TABLE habitacion_estandar (
+CREATE TABLE reservas (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(100) NOT NULL,
     apellido VARCHAR(100),
     cedula VARCHAR(20),
     telefono VARCHAR(20),
     correo VARCHAR(100),
-    activo boolean
-);
-
-CREATE TABLE habitacion_suite (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR(100) NOT NULL,
-    apellido VARCHAR(100),
-    cedula VARCHAR(20),
-    telefono VARCHAR(20),
-    correo VARCHAR(100),
-    activo boolean
-);
-
-CREATE TABLE habitacion_premium (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR(100) NOT NULL,
-    apellido VARCHAR(100),
-    cedula VARCHAR(20),
-    telefono VARCHAR(20),
-    correo VARCHAR(100),
+    tipo_habitacion varchar(100) NOT NULL,
     activo boolean
 );
 
 -- Inserción de Datos de Ejemplo (con 'activo' establecido)
-INSERT INTO habitacion_estandar (nombre, apellido, cedula, telefono, correo, activo) VALUES
-('Ana', 'García', '12345678', '111222333', 'ana.garcia@example.com', TRUE),
-('Carlos', 'López', '87654321', '444555666', 'carlos.lopez@example.com', TRUE);
-
-INSERT INTO habitacion_suite (nombre, apellido, cedula, telefono, correo, activo) VALUES
-('Laura', 'Martínez', '98761234', '777888999', 'laura.martinez@example.com', TRUE),
-('Pedro', 'Rodríguez', '43219876', '101112131', 'pedro.rodriguez@example.com', TRUE);
-
-INSERT INTO habitacion_premium (nombre, apellido, cedula, telefono, correo, activo) VALUES
-('Sofía', 'Pérez', '11223344', '141516171', 'sofia.perez@example.com', TRUE),
-('Diego', 'Sánchez', '44332211', '181920212', 'diego.sanchez@example.com', TRUE);
+INSERT INTO reservas (nombre, apellido, cedula, telefono, correo, tipo_habitacion, activo) VALUES
+('Ana', 'García', '12345678', '111222333', 'ana.garcia@example.com', 'Estandar', TRUE),
+('Carlos', 'López', '87654321', '444555666', 'carlos.lopez@example.com', 'Suite', TRUE),
+('Laura', 'Martínez', '98761234', '777888999', 'laura.martinez@example.com', 'Suite', TRUE),
+('Pedro', 'Rodríguez', '43219876', '101112131', 'pedro.rodriguez@example.com', 'Premium', TRUE),
+('Sofía', 'Pérez', '11223344', '141516171', 'sofia.perez@example.com', 'Estandar', TRUE),
+('Diego', 'Sánchez', '44332211', '181920212', 'diego.sanchez@example.com', 'Premium', TRUE);
