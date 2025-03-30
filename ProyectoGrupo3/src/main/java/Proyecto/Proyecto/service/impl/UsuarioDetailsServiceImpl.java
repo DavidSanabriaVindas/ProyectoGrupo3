3b8cefed78296ac528ebd0fef26470830a16e0a2
santiOM0611 <sorozco80826@ufide.ac.cr>
2025-03-30 17:30:36 -0600

    Seguridad
    Clases para implementar la seguridad

---
+++ ProyectoGrupo3/src/main/java/Proyecto/Proyecto/service/impl/UsuarioDetailsServiceImpl.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package Proyecto.Proyecto.service.impl;
+
+/**
+ *
+ * @author orozc
+ */
+public class UsuarioDetailsServiceImpl {
+
+}
