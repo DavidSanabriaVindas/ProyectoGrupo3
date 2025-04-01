630d465af8a055867b5d6a50a9afb44bbf0fa357
DavidSanabriaVindas <dsanabria30061@ufide.ac.cr>
2025-03-31 20:34:01 -0600

    avance cambios logsig

--- ProyectoGrupo3/src/main/java/Proyecto/Proyecto/service/UsuarioDetailsService.java
+++ ProyectoGrupo3/src/main/java/Proyecto/Proyecto/service/UsuarioDetailsService.java
@@ -1,14 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
-
 package Proyecto.Proyecto.service;
 
-/**
- *
- * @author orozc
- */
-public interface UsuarioDetailsService {
+import org.springframework.security.core.userdetails.*;
 
+public interface UsuarioDetailsService {
+    
+     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException;
 }
