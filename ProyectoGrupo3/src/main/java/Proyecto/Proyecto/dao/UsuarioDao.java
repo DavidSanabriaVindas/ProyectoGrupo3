41c94c3e4df7df5d67996e29030adb133d4a468a
DavidSanabriaVindas <dsanabria30061@ufide.ac.cr>
2025-04-01 22:45:39 -0600

    .

--- ProyectoGrupo3/src/main/java/Proyecto/Proyecto/dao/UsuarioDao.java
+++ ProyectoGrupo3/src/main/java/Proyecto/Proyecto/dao/UsuarioDao.java
@@ -1,10 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+
 package Proyecto.Proyecto.dao;
 
-import Proyecto.Proyecto.domain.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsuarioDao extends JpaRepository<Usuario, Long>{
-    
-    Usuario findByUsername(String username);
+/**
+ *
+ * @author orozc
+ */
+public interface UsuarioDao {
 
 }
